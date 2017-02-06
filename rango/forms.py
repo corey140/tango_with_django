f9e4b3ae9bbb4c42365d7a5cf54097a88f3c663e
Corey <2191470m@student.gla.ac.uk>
2017-02-06 15:53:10 +0000

    Creating login template in chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
 	name = forms.CharField(max_length=128,help_text="Please enter the category name.")
@@ -12,8 +13,6 @@ class CategoryForm(forms.ModelForm):
 		fields = ('name',)
 		
 class PageForm(forms.ModelForm):
-	
-
 	title = forms.CharField(max_length=128,help_text="Please enter the category name.")
 	url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -30,3 +29,15 @@ class PageForm(forms.ModelForm):
 			cleaned_data['url'] = url
 			
 			return cleaned_data
+			
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput())
+	
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
+		
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		fields = ('website','picture')
